385ee3872d74f5def08bba36887e470a4b13b36c
Lalit <ldas@gainsight.com>
2022-03-07 14:19:08 +0530

    2nd commit

--- src/main/java/com/wealth/stock/StockApplication.java
+++ src/main/java/com/wealth/stock/StockApplication.java
@@ -5,10 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableMongoRepositories
 @EnableScheduling
 public class StockApplication {
+
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockApplication.class, args);
