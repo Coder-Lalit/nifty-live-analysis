1294eaae0608efcab1e51fe68e0b6cc9ca76536f
Lalit <ldas@gainsight.com>
2022-03-15 00:05:05 +0530

    2nd commit

--- src/main/java/com/wealth/stock/controller/ping.java
+++ src/main/java/com/wealth/stock/controller/ping.java
@@ -1,12 +1,22 @@
 package com.wealth.stock.controller;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static io.restassured.RestAssured.given;
 
 @RestController
 public class ping {
     @GetMapping("/ping")
     public String getStatus(){
         return "I'm Nifty app and Up";
+    }
+
+    @Scheduled(fixedDelay = 1200000)
+    public void checkStatus(){
+        System.out.println(
+                given().when().get("https://nifty-live-analysis.herokuapp.com/ping").then().extract().asString()
+        );
     }
 }
